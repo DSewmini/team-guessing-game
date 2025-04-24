e17cce92a7bbdef490070e2cbe604818c4bccad4
DSewmini <dakshinasewminiarambepola@gmail.com>
2025-04-24 13:01:10 +0530

    Add starter code

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+import random
+number = random.random(1,10)
+print("Guess a number between 1 and 10")
+guess = int(input())
+
+if guess == number:
+    print("You win!")
+else:
+    print(f"Wrong!The number was {number}")
