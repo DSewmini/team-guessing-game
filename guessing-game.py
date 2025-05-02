16fe5bf608d4e334d3da19368ee5bcc395580c40
DSewmini <dakshinasewminiarambepola@gmail.com>
2025-05-03 02:00:53 +0530

    corrected

--- guessing-game.py
+++ guessing-game.py
@@ -9,9 +9,9 @@ while True:
     if guess == number:
         print("You win!")
     else:
-        if guess < number;
+        if guess < number:
         print("Tool low"
-              elif guess > number;
+              elif guess > number:
               print("Tool high!")
         print(f"Wrong!The number was {number}")
     
